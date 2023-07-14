d19abd30976051f610f3e1b3a1e6bc41913942ba
Gaelle <gaellee.valmir@gmail.com>
2023-07-14 10:36:29 -0400

    Initial Commit

---
+++ githubpartners/githubpartnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubpartnersApp.swift
+//  githubpartners
+//
+//  Created by Gaelle Valmir on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct githubpartnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
